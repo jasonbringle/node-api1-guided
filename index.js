e54030aa079237eab3b9fe08ea4bf0be7a1bda33
jasonbringle <jasonbringle@gmail.com>
2020-07-07 20:42:52 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const shortid = require('shortid');
+
+const server = express();
+
+let hubs = [];
+
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.json({hello: 'world'});
+});
+
+server.get('/hello', (reg, res) => {
+    res.json({hello: 'LambdaSchool'})
+})
+
+server.post('/api/hubs', (req, res) => {
+    const hubInfo = req.body;
+    hubInfo.id = shortid.generate();
+
+    hubs.push(hubInfo);
+
+    res.status(201).json(hubInfo);
+
+})
+
+server.get('/api/hubs', (req,res) => {
+    res.json(hubs);
+})
+
+server.delete('/api/hubs/:id', (res,req) => {
+    const {id} = req.params;
+
+    const deleted = hubs.find(hub => hub.id == id);
+    
+    if(deleted) {
+        hubs = hubs.filter(hub => hub.id !== id);
+        res.status(200).json(deleted);
+    } else {
+        res.status(404).json({message: "hub not found"})
+    }
+});
+
+// Update-Change
+server.patch("/api/hubs/:id" , (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+
+    let found = hubs.find(hub => hub.id === id)
+    if(found) {
+        Object.assign(found, changes);
+        res.status(200).json(found);
+    } else {
+        res.status(404).json({ message: 'hub id not found'})
+    }
+})
+
+// Update-replace
+server.put("/api/hubs/:id" , (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    changes.id = id;
+
+    let index = hubs.findIndex(hub => hub.id === id)
+    if (index !== -1) {
+        hubs[index] = changes;
+        res.status(200).json(hubs[index]);
+    } else {
+        res.status(404).json({ message: 'hub id not found'})
+    }
+})
+
+
+const PORT = 5000
+server.listen(PORT, () => {
+    console.log('listening on localhost:', 5000);
+})
